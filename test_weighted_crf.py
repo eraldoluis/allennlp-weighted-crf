6eef858e72e4a07dc0bc43dd3bf3520311f7f718
Eraldo R. Fernandes <eraldoluis@gmail.com>
2022-03-09 22:24:48 +0100

    Minor mods (not relevant).

--- test_weighted_crf.py
+++ test_weighted_crf.py
@@ -9,13 +9,11 @@ config_file = "training_config/ner_new.json"
 # Use overrides to train on CPU.
 overrides = json.dumps(
     {
-        "train_data_path": "~/lia/src/allennlpdev/allennlp-models/test_fixtures/tagging/conll2003.txt",
-        "validation_data_path": "~/lia/src/allennlpdev/allennlp-models/test_fixtures/tagging/conll2003.txt",
-        "trainer": {
-            # "cuda_device": -1, 
-            "validation_metric": "+f1-measure-overall"
-        },
-        "model": {"verbose_metrics": True}
+        "train_data_path": "../allennlp-models/test_fixtures/tagging/conll2003.txt",
+        "validation_data_path": "../allennlp-models/test_fixtures/tagging/conll2003.txt",
+        "trainer.cuda_device": -1, 
+        # "trainer.validation_metric": "+f1-measure-overall", 
+        # "model.verbose_metrics": True
     })
 
 serialization_dir = "train_out"
@@ -25,7 +23,7 @@ serialization_dir = "train_out"
 # over and over again for debugging purposes, it will.
 # Hence we wipe it out in advance.
 # BE VERY CAREFUL NOT TO DO THIS FOR ACTUAL TRAINING!
-# shutil.rmtree(serialization_dir, ignore_errors=True)
+shutil.rmtree(serialization_dir, ignore_errors=True)
 
 # Assemble the command into sys.argv
 sys.argv = [
